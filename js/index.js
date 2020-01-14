2e36e870036bdfd4b7b1de33a4e86ec3c62a4445
VodeniZeko <salexy.es@gmail.com>
2020-01-14 13:30:26 -0800

    second push

--- js/index.js
+++ js/index.js
@@ -1,1 +1,35 @@
 // Your code goes here
+
+const funBus = document.querySelector(".logo-heading");
+funBus.addEventListener("mouseenter", () => {
+  gsap.to(".logo-heading", {
+    duration: 1,
+    rotate: 180,
+    color: "#fff",
+    yoyo: true,
+    y: 150,
+    x: 300,
+    repeat: 1
+  });
+});
+
+const home = document.querySelector("body");
+home.addEventListener("keydown", e => {
+  if (e.keyCode === 38) {
+    // if key arrow up
+    gsap.to("body", {
+      duration: 0.1,
+      background: "#008000",
+      yoyo: true,
+      repeat: 1
+    });
+  } else {
+    // else key arrow down
+    gsap.to("body", {
+      duration: 0.1,
+      background: "#FF0000",
+      yoyo: true,
+      repeat: 1
+    });
+  }
+});
