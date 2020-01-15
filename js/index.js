b259e6ec2fc1e5bb556a56ae4e7ccbea90429624
VodeniZeko <salexy.es@gmail.com>
2020-01-14 16:17:20 -0800

    funfun

--- js/index.js
+++ js/index.js
@@ -2,6 +2,7 @@
 
 const funBus = document.querySelector(".logo-heading");
 funBus.addEventListener("mouseenter", () => {
+  // hovering over logo text rotates it and color is changed
   gsap.to(".logo-heading", {
     duration: 1,
     rotate: 180,
@@ -15,6 +16,7 @@ funBus.addEventListener("mouseenter", () => {
 
 const home = document.querySelector("body");
 home.addEventListener("keydown", e => {
+  // pressing key arrows changes the body color
   if (e.keyCode === 38) {
     // if key arrow up
     gsap.to("body", {
@@ -34,13 +36,11 @@ home.addEventListener("keydown", e => {
   }
 });
 
-const text = document.querySelectorAll("div > h2");
-const para = document.querySelectorAll("header > p");
-
 var tl = gsap.timeline({ defaults: { duration: 1, ease: "none" } });
 
 window.addEventListener("load", () => {
   tl.to("div > h2", {
+    // on load text grows and changes color
     duration: 3,
     font: 50,
     color: "#660000"
@@ -50,14 +50,14 @@ window.addEventListener("load", () => {
 const img1 = document.querySelector("div > img:nth-child(1)");
 const img2 = document.querySelector(".img-fluid");
 img1.addEventListener("wheel", e => {
-  e.preventDefault();
+  e.preventDefault(); // preventing scrolling the website while scalling the image
   let scale = 1;
   scale = Math.min(Math.max(0.125, scale), 4);
   scale += event.deltaY * -0.01;
   img1.style.transform = `scale(${scale})`;
 });
 img2.addEventListener("wheel", e => {
-  e.preventDefault();
+  e.preventDefault(); //preventing scrolling the website while scalling the image
   let scale = 1;
   scale = Math.min(Math.max(0.125, scale), 4);
   scale += event.deltaY * -0.01;
@@ -68,29 +68,51 @@ const bus = document.querySelector("header > img");
 
 window.addEventListener("resize", e => {
   gsap.to("header > img", {
+    // resizing the browser skews the image
     skewY: -180,
     skewX: 180
   });
 });
 
 window.scrollTo({
+  //scroll on load
   top: 300,
   left: 100,
   behavior: "smooth"
 });
 
 const a = document.querySelectorAll("p");
-Array.from(a).forEach(item =>
-  item.addEventListener("click", () => {
+const div = document.querySelectorAll(".text-content");
+console.log(div);
+
+Array.from(div).forEach(el =>
+  el.addEventListener("click", () => {
+    // on click changes background color (we are going to prevent this if clicking on paragraph)
+    el.style.background = "gray";
+  })
+);
+Array.from(a).forEach((
+  item //loop over the p and add event listener
+) =>
+  item.addEventListener("click", e => {
     item.style.color = "#fff";
+    e.stopPropagation(); //div is not changing background to gray
   })
 );
 
 const wl = document.querySelector("header > h2");
 wl.addEventListener("dblclick", e => {
+  //click twice to flip the h2
   gsap.to("header > h2", {
-    rotateX: -200,
-    yoyo: true,
-    repeat: 1
+    rotateX: -200
   });
 });
+
+const navLink = document.querySelectorAll("a");
+Array.from(navLink).forEach((
+  el // links are taking you to KNOWHERE
+) =>
+  el.addEventListener("click", e => {
+    e.preventDefault();
+  })
+);
